84ead2a1f0b26d595b9575ad0227ca319b98804c
PerdidoenCoding <willianlinchang@gmail.com>
2024-01-16 21:56:56 +0800

    Added model functions and mongoDB url connection

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,33 @@
 const http = require('http');
+const mongoose = require('mongoose');
 
 const app = require('./app');
 
+const { loadPlanetsData } = require('./models/planets.model');
+const { loadLaunchData } = require('./models/launches.model');
+
 const PORT = process.env.PORT || 8000;
+
+const MONGO_URL = 'mongodb+srv://willianchang:e9hTGpuJg6TCQbJP@nasa-project.ek4lumv.mongodb.net/?retryWrites=true&w=majority'
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}...`)
+mongoose.connection.on('open', () => {
+    console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error(err);
 })
+
+async function startServer() {
+    await mongoose.connect(MONGO_URL);
+    await loadPlanetsData();
+    await loadLaunchData();
+
+    server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+    });
+}
+
+startServer();
